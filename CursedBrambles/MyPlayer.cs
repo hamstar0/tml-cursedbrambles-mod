ed18bb89f9a036a0e5c9d1ef8c01384c6f1db721
hamstar0 <webutek@gmail.com>
2020-11-14 01:43:14 -0800

    v0.1.0 - Implemented 'underground only' player brambles * Swapped MyNPC.cs with MyPlayer.cs * Implemented 'underground only' player brambles

--- CursedBrambles/MyPlayer.cs
+++ CursedBrambles/MyPlayer.cs
@@ -1,31 +1,65 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using HamstarHelpers.Services.Timers;
+using HamstarHelpers.Helpers.World;
 using CursedBrambles.Tiles;
 
 
 namespace CursedBrambles {
-	class CursedBramblesNPC : GlobalNPC {
-		public override void AI( NPC npc ) {
-			if( npc.boss ) {
-				var config = CursedBramblesConfig.Instance;
+	class CursedBramblesPlayer : ModPlayer {
+		private Vector2 OldPosition = default( Vector2 );
 
-				if( config.Get<bool>( nameof(config.BossesCreateBrambleTrail) ) ) {
-					this.CreateCursedBrambleTrail( npc );
-				}
+
+
+		////////////////
+
+		public override void PreUpdate() {
+			if( Main.netMode != NetmodeID.MultiplayerClient ) {
+				this.PreUpdateHost();
 			}
+		}
+
+		private void PreUpdateHost() {
+			var config = CursedBramblesConfig.Instance;
+			if( config.Get<bool>( nameof( config.PlayersCreateBrambleTrail ) ) ) {
+				this.UpdatePlayerBrambleTrail();
+			}
+		}
+
+		////
+
+		private void UpdatePlayerBrambleTrail() {
+			if( (int)(this.player.position.Y / 16f) < WorldHelpers.UnderworldLayerTopTileY ) {
+				return;
+			}
+
+			string timerName = "CursedBramblePlayerTrail_" + this.player.whoAmI;
+			if( Timers.GetTimerTickDuration(timerName) > 0 ) {
+				return;
+			}
+
+			Timers.SetTimer( timerName, 60 * 5, false, () => {
+				if( this.OldPosition != default( Vector2 ) ) {
+					this.CreateCursedBrambleTrail( this.OldPosition );
+				}
+
+				this.OldPosition = this.player.Center;
+				return false;
+			} );
 		}
 
 
 		////
 
-		private void CreateCursedBrambleTrail( NPC npc ) {
-			int tileX = (int)npc.Center.X / 16;
-			int tileY = (int)npc.Center.Y / 16;
-			int thickness = npc.width > npc.height ? npc.width : npc.height;
-			thickness = (int)Math.Ceiling( (double)thickness / 16d );
+		private void CreateCursedBrambleTrail( Vector2 pos ) {
+			Player plr = this.player;
+			int tileX = (int)pos.X / 16;
+			int tileY = (int)pos.Y / 16;
 
-			CursedBrambleTile.CreateBramblePatchAt( tileX, tileY, thickness, 0.05f );
+			CursedBrambleTile.CreateBramblePatchAt( tileX, tileY, 16, 0.05f );
 		}
 	}
 }
